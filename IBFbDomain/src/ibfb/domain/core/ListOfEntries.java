384caf10737087fbfe368593de391e5afb16e2fd
tmsanchez <T.M.Sanchez@cgiar.org>
2012-06-25 18:32:54 +0000

    Se agrega método para que el numero de columnas en la lista de germoplasma sea igual al numero de factores

--- IBFbDomain/src/ibfb/domain/core/ListOfEntries.java
+++ IBFbDomain/src/ibfb/domain/core/ListOfEntries.java
@@ -8,30 +8,27 @@ import java.util.List;
  * @author TMSANCHEZ
  */
 public class ListOfEntries {
-        // Constants for list column
+    // Constants for list column
+
     public static final String LIST_ID = "LISTID";
     public static final String LIST_NAME = "LIST NAME";
     public static final String GID = "GID";
     public static final String DESIG = "DESIG";
     public static final String DESIGNATION = "DESIGNATION";
     public static final String ENTRY_CD = "ENTRY CD";
-    public static final String ENTRY_CODE = "ENTRY CODE";    
+    public static final String ENTRY_CODE = "ENTRY CODE";
     public static final String SOURCE = "SOURCE";
     public static final String GRPNAME = "GRPNAME";
     public static final String ENTRYID = "ENTRYID";
     public static final String LREDID = "LREDID";
     public static final String ENTRY_NUMBER = "ENTRY_NUMBER";
-    public static final String ENTRY = "ENTRY";   
-    
-    
+    public static final String ENTRY = "ENTRY";
     public static final String GERMPLASM_ENTRY_NUMBER = "GERMPLASMENTRYNUMBER";
     public static final String GERMPLASM_ENTRY_CODE = "GERMPLASMENTRYCODE";
     public static final String GERMPLASM_ID_DBCV = "GERMPLASMIDDBCV";
     public static final String GERMPLASM_ID_DBID = "GERMPLASMIDDBID";
     public static final String SEED_SOURCE_NAME = "SEEDSOURCENAME";
     public static final String CROSS_HISTORY_PEDIGREE_STRING = "CROSSHISTORYPEDIGREESTRING";
-    
-
     private Integer gid;
     private String entryCode;
     private String designation;
@@ -44,7 +41,6 @@ public class ListOfEntries {
     public ListOfEntries() {
     }
 
-
     public ListOfEntries(Integer gid, String entryCode, String designation, String cross, String source, String uniqueId, Integer entryId) {
         this.gid = gid;
         this.entryCode = entryCode;
@@ -56,31 +52,66 @@ public class ListOfEntries {
     }
 
     /**
-     * Get the list of values according to combination of PROPERTY + SCALE
-     * It check if columnName matches with predefined values, if match OK
-     * then assign the value from entries properties (gid, entry code, etc)
+     * Get the list of values according to combination of PROPERTY + SCALE It
+     * check if columnName matches with predefined values, if match OK then
+     * assign the value from entries properties (gid, entry code, etc)
+     *
      * @param columnList Column headers list with header to display
      * @return a list of values for each column
      */
     public List<Object> getColumnValues(List<String> columnList) {
-       List<Object> columnValues = new ArrayList<Object>();
-       for (String columnName: columnList) {
-           if (columnName.equals(GERMPLASM_ENTRY_NUMBER)) {
-             columnValues.add(number);  
-           } else  if (columnName.equals(GERMPLASM_ENTRY_CODE)) {
-               columnValues.add(entryCode);
-           } else  if (columnName.equals(GERMPLASM_ID_DBCV)) {
-               columnValues.add(designation);
-           } else  if (columnName.equals(GERMPLASM_ID_DBID)) {
-               columnValues.add(gid);
-           } else  if (columnName.equals(SEED_SOURCE_NAME)) {
-               columnValues.add(source);
-           } else  if (columnName.equals(CROSS_HISTORY_PEDIGREE_STRING)) {
-               columnValues.add(cross);
-           }
-           
-       }
-       return columnValues;
+        List<Object> columnValues = new ArrayList<Object>();
+        for (String columnName : columnList) {
+            if (columnName.equals(GERMPLASM_ENTRY_NUMBER)) {
+                columnValues.add(number);
+            } else if (columnName.equals(GERMPLASM_ENTRY_CODE)) {
+                columnValues.add(entryCode);
+            } else if (columnName.equals(GERMPLASM_ID_DBCV)) {
+                columnValues.add(designation);
+            } else if (columnName.equals(GERMPLASM_ID_DBID)) {
+                columnValues.add(gid);
+            } else if (columnName.equals(SEED_SOURCE_NAME)) {
+                columnValues.add(source);
+            } else if (columnName.equals(CROSS_HISTORY_PEDIGREE_STRING)) {
+                columnValues.add(cross);
+            } else {
+                columnValues.add("");;
+            }
+
+        }
+        return columnValues;
+    }
+
+    /**
+     * Get the list of values according to combination of PROPERTY + SCALE It
+     * check if columnName matches with predefined values, if match OK then
+     * assign the value from entries properties (gid, entry code, etc)
+     *
+     * @param columnList Column headers list with header to display
+     * @return a list of values for each column
+     */
+    public List<Object> getColumnValuesFromFactors(List<Factor> localFactors) {
+        List<Object> columnValues = new ArrayList<Object>();
+        for (Factor factor : localFactors) {
+            String columnName = Workbook.getStringWithOutBlanks(factor.getProperty()+factor.getScale());
+            if (columnName.equals(GERMPLASM_ENTRY_NUMBER)) {
+                columnValues.add(number);
+            } else if (columnName.equals(GERMPLASM_ENTRY_CODE)) {
+                columnValues.add(entryCode);
+            } else if (columnName.equals(GERMPLASM_ID_DBCV)) {
+                columnValues.add(designation);
+            } else if (columnName.equals(GERMPLASM_ID_DBID)) {
+                columnValues.add(gid);
+            } else if (columnName.equals(SEED_SOURCE_NAME)) {
+                columnValues.add(source);
+            } else if (columnName.equals(CROSS_HISTORY_PEDIGREE_STRING)) {
+                columnValues.add(cross);
+            } else {
+                columnValues.add("");;
+            }
+
+        }
+        return columnValues;
     }
 
     public String getCross() {
@@ -147,12 +178,8 @@ public class ListOfEntries {
         this.number = number;
     }
 
-
-    
     @Override
     public String toString() {
         return "ListEntry{" + "gid=" + gid + "entryCode=" + entryCode + "designation=" + designation + "cross=" + cross + "source=" + source + "uniqueId=" + uniqueId + "entryId=" + entryId + '}';
     }
-
-    
 }
