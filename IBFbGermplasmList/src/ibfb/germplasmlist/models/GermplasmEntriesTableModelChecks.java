62672f9dc47665c14b845beff6e689d79bb6c78f
tmsanchez <T.M.Sanchez@cgiar.org>
2012-09-20 18:44:11 +0000

    Fix for IBPSupport-84, 31 and 102 and 71

--- IBFbGermplasmList/src/ibfb/germplasmlist/models/GermplasmEntriesTableModelChecks.java
+++ IBFbGermplasmList/src/ibfb/germplasmlist/models/GermplasmEntriesTableModelChecks.java
@@ -1,4 +1,3 @@
-
 package ibfb.germplasmlist.models;
 
 import ibfb.domain.core.Factor;
@@ -6,50 +5,43 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.swing.table.AbstractTableModel;
 
-
 public class GermplasmEntriesTableModelChecks extends AbstractTableModel {
 
-    private boolean hasChecks=false;
+    private boolean hasChecks = false;
     private List<Factor> factorHeaders;
-    private List<List<Object>> germplasmData;    
-    private String[] checkHeaders={"Initial position","Frequency"};
-    private boolean withColor=false;
+    private List<List<Object>> germplasmData;
+    private String[] checkHeaders = {"Initial position", "Frequency"};
+    private boolean withColor = false;
 
     public GermplasmEntriesTableModelChecks() {
         clearTable();
     }
-    
-    public GermplasmEntriesTableModelChecks(List<Factor> factorHeaders,List<List<Object>> germplasmData) {
+
+    public GermplasmEntriesTableModelChecks(List<Factor> factorHeaders, List<List<Object>> germplasmData) {
         this.factorHeaders = factorHeaders;
         this.germplasmData = germplasmData;
-      
-    }
 
+    }
 
     @Override
     public boolean isCellEditable(int rowIndex, int columnIndex) {
-       
-        
-        if(!hasChecks){
+
+
+        if (!hasChecks) {
             return false;
-        }else{
-            if(columnIndex>factorHeaders.size()-1){
+        } else {
+            if (columnIndex > factorHeaders.size() - 1) {
                 return false;  //true if position and frequency will be editables
             }
         }
-        
+
         return super.isCellEditable(rowIndex, columnIndex);
     }
 
-    
-    
-    
-    public void setHasChecks(boolean hasChk){
-        this.hasChecks=hasChk;
+    public void setHasChecks(boolean hasChk) {
+        this.hasChecks = hasChk;
     }
-    
-    
-    
+
     @Override
     public int getRowCount() {
         return germplasmData.size();
@@ -57,10 +49,10 @@ public class GermplasmEntriesTableModelChecks extends AbstractTableModel {
 
     @Override
     public int getColumnCount() {
-        if(!hasChecks){
-        return factorHeaders.size();}
-        else{
-            return factorHeaders.size()+2;
+        if (!hasChecks) {
+            return factorHeaders.size();
+        } else {
+            return factorHeaders.size() + 2;
         }
     }
 
@@ -83,30 +75,29 @@ public class GermplasmEntriesTableModelChecks extends AbstractTableModel {
 
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
-         List<Object> columnValues = germplasmData.get(rowIndex);
-         return columnValues.get(columnIndex);
+        List<Object> columnValues = germplasmData.get(rowIndex);
+        return columnValues.get(columnIndex);
     }
 
     @Override
     public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
-       
-         if(hasChecks){
-                      
-               List<Object> columnValues = germplasmData.get(rowIndex);
-               
-               columnValues.set(columnIndex, aValue);
-               
-        
-        fireTableCellUpdated(rowIndex, columnIndex);
+
+        if (hasChecks) {
+
+            List<Object> columnValues = germplasmData.get(rowIndex);
+
+            columnValues.set(columnIndex, aValue);
+
+
+            fireTableCellUpdated(rowIndex, columnIndex);
+        } else {
+            
         }
-        
-        
-        
+
+
+
     }
 
-    
-    
-    
     public List<Factor> getFactorHeaders() {
         return factorHeaders;
     }
@@ -114,12 +105,10 @@ public class GermplasmEntriesTableModelChecks extends AbstractTableModel {
     public List<List<Object>> getGermplasmData() {
         return germplasmData;
     }
-    
+
     public void clearTable() {
-        factorHeaders = new ArrayList<Factor> ();
+        factorHeaders = new ArrayList<Factor>();
         germplasmData = new ArrayList<List<Object>>();
         fireTableDataChanged();
     }
-    
-    
 }
