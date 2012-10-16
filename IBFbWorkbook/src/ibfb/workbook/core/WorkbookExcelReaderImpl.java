8e39d921d52bd63f35e80e1748a489ee33a9f41f
tmsanchez <T.M.Sanchez@cgiar.org>
2012-10-16 19:15:16 +0000

    Changes beta 21

--- IBFbWorkbook/src/ibfb/workbook/core/WorkbookExcelReaderImpl.java
+++ IBFbWorkbook/src/ibfb/workbook/core/WorkbookExcelReaderImpl.java
@@ -116,7 +116,7 @@ public class WorkbookExcelReaderImpl implements WorkbookExcelReader {
              }else{
                 theCrop = OTHERCROPS; 
              }          
-        theCrop=MAIZE; 
+        theCrop=WHEAT; 
         
         return theCrop;
     }
